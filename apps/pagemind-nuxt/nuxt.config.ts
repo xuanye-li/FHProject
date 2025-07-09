7afa4414d411db190d310285c8a63f23c97af501
Xuanye Li <xuan.yeah.li@gmail.com>
2025-07-09 18:30:26 -0400

    cleaned files

--- apps/pagemind-nuxt/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  ssr: false,
-  app: {
-    baseURL: '/',           // Change from './' to '/'
-    buildAssetsDir: '/_nuxt/'
-  },
-  nitro: { preset: 'static' }     // Output is fully static SPA
-})
